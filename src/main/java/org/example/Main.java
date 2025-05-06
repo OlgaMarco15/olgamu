d21d1a44ca5b86b24f496365e81bd27cd38cb399
OlgaMarco15 <olgamarcougarte@gmail.com>
2025-05-06 12:09:30 +0200

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main (String args[]) {
         double n1=0;
-        double b=0;
+        double n2=0;
         double resultado;
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese el valor de n1: ");
         n1 = sc.nextDouble();
         System.out.println("Ingrese el valor de b: ");
-        b = sc.nextDouble();
-        resultado = multiplica2(n1, b);
+        n2 = sc.nextDouble();
+        resultado = multiplica2(n1, n2);
         System.out.println("El resultado de la multiplicación es: " + resultado);
 
     }
