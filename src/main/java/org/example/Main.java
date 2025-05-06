12946e920ef71c525aba54cc11aed93c85f8d153
olgam <olga.marco@educa.madrid.org>
2025-05-06 12:33:25 +0200

    Práctica Entornos de Desarrollo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,7 @@ public class Main {
         n2 = sc.nextDouble();
         resultado = multiplica2(n1, n2);
         System.out.println("El resultado de la multiplicación es: " + resultado);
+        System.out.println("Modificacion tras la rama");
 
     }
 
