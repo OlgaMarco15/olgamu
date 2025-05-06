9b98f9347266a5328e09c65ab3e42ca65d0f59a4
olgam <olga.marco@educa.madrid.org>
2025-05-06 12:02:51 +0200

    Práctica Entornos de Desarrollo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,24 +3,22 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        double a=0;
+    public static void main (String args[]) {
+        double n1=0;
         double b=0;
         double resultado;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el valor de a: ");
-        a = sc.nextDouble();
+        System.out.println("Ingrese el valor de n1: ");
+        n1 = sc.nextDouble();
         System.out.println("Ingrese el valor de b: ");
         b = sc.nextDouble();
-        resultado = multiplica(a, b);
+        resultado = multiplica2(n1, b);
         System.out.println("El resultado de la multiplicación es: " + resultado);
 
     }
 
-    private static double multiplica ( double a, double b) {
 
+    private static double multiplica2 ( double a, double b) {
         return a*b;
     }
-
-
 }
